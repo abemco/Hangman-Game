99d9336a8734cea3e57a7024f4876113cde7f630
abemco <abemco@aol.com>
2018-03-02 06:46:47 -0500

    add javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,50 @@
 // Array of words
-const word = ['Paris', 'Amsterdam', 'Miami', 'New York', 'Sydney', 'London', 'Singapore', 'Orlando', 'Rio de Janeiro']
+const word = ['paris', 'amsterdam', 'miami', 'new york', 'sydney', 'london', 'singapore', 'orlando', 'rio de janeiro']
 // Choose word randomly
 let randNum = Math.floor(Math.random() * word.length);
 let choosenWord = word[randNum];
-console.log(choosenWord);
+let rightWord = [];
+let wrongWord = [];
 let underScore = [];
+
+let docUnderScore = document.getElementsByClassName('underScore');
+let docRightGuess = document.getElementsByClassName('rightGuess');
+let docWrongGuess = document.getElementsByClassName('wrongGuess');
+
 console.log(choosenWord);
+
 // Create underscores based on lenght of word
-let generateunderscore = () => {
+let generateunderScore = () => {
     for(let i = 0; i < choosenWord.length; i++) {
         underScore.push('_');
     }
     return underScore;
 }
 
-console.log(generateunderscore());
+console.log(generateunderScore());
 // Get users guess
 document.addEventListener('keypress', (event) => {
-    let keyWord = string.fromCharCode(event.keyCode);
-    console.log(keyword);
+    let keyWord = String.fromCharCode(event.keyCode);
+//  If users guess is right
+    if (choosenWord.indexOf(keyWord) > -1) {
+      // add to right words array
+      rightWord.push(keyWord);
+      docUnderScore[0].innerHTML = underScore.join('');
+    //   replace underscore with right letter
+      underScore[choosenWord.indexOf(keyWord)] = keyWord
+        docUnderScore[0].innerHTML = underScore.join(' ');
+        docRightGuess[0].innerHTML = rightWord;
+        if (underScore.join('') == choosenWord) {
+            alert('You Win');
+        }
+    } else {
+         wrongWord.push(keyWord);
+         docWrongGuess[0].innerHTML = wrongWord;
+    }
 });
+
+innerHTML = generateunderScore().join(' ');
+
 // Check if guess is right
 // If right push to right array
 // If wrong push to wrong array
